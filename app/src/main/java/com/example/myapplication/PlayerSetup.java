c872beb46a2023b5524a8f53a9b6ce5e8b8a8025
NaMan6122 <namang2510@gmail.com>
2024-07-04 16:26:20 +0530

    GameDisplay page

--- app/src/main/java/com/example/myapplication/PlayerSetup.java
+++ app/src/main/java/com/example/myapplication/PlayerSetup.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class PlayerSetup extends AppCompatActivity {
+
+    private EditText player1;
+    private EditText player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player_setup);
+
+        //assigning the player ids to the variables.
+        player1 = findViewById(R.id.editTextText);
+        player2 = findViewById(R.id.editTextText2);
+    }
+
+    public void submitButtonClick(View view){
+        String player1Name = player1.getText().toString();
+        String player2Name = player2.getText().toString();
+        Intent intent = new Intent(this, GameDisplay.class);
+        intent.putExtra("Player Names", new String[] {player1Name, player2Name});
+        startActivity(intent);
     }
 }
